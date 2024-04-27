eac3637330b0fab57111a386f418476f3709a9b3
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-04-27 03:24:05 +0200

    mod apy

--- js/details.js
+++ js/details.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(parametro);
     const url = `https://striveschool-api.herokuapp.com/api/product/${parametro}`;
     const token =
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFhNjAxMDE4N2U1YzAwMTgxNGM3MjgiLCJpYXQiOjE3MDU2NjQ1MjgsImV4cCI6MTcwNjg3NDEyOH0.m6j18AqBGfu3uqENFWj6gDgb6LpOFg0ke6rCAt4BuvM";
+      "Bearer Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjJjNTJmMjMzNzk3OTAwMTlhNjcyMzkiLCJpYXQiOjE3MTQxODA4NTAsImV4cCI6MTcxNTM5MDQ1MH0.FHAWvAUKvnGsdtILksmo4lmFqVxiVBeqT2zEkHPrcsU";
 
     fetch(url, {
       headers: {
